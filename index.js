e54319ff94e56b513feb7823d18c822f6c4f96dc
alepri51 <39708925+alepri51@users.noreply.github.com>
2019-02-06 19:01:26 +0300

    commit

--- index.js
+++ index.js
@@ -14,5 +14,20 @@ const router = Router({ Classes });
 app.use('/api', router);
 
 app.listen(8001, function () {
-  console.log('Example app listening on port 8001!');
+    console.log('Example app listening on port 8001!');
+
+    const axios = require('axios');
+    (async() => {
+
+        for(let i = 1; i <= 100; i++) {
+            let response = await axios.get('http://localhost:8001/api/custom.get').catch(err => console.error(i, err));
+            response && console.info(i, response.data);
+            //await sleep(95);
+        }
+    })();
 });
+
+const sleep = (ms = 1000) => new Promise(resolve => {
+    setTimeout(() => resolve(200), ms);
+})
+
