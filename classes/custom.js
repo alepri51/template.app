7188823c19cd209ea4bac99cc811a122552e32f5
ap51 <alexander.privaloff@gmail.com>
2019-02-07 01:43:27 +0300

    commit

--- classes/custom.js
+++ classes/custom.js
@@ -1,9 +1,34 @@
+const path = require('path');
+
 const { Classes } = require('template.api');
 const { API, SecuredAPI } = Classes();
 
 class Custom extends SecuredAPI {
     constructor(...args) {
         super(...args);
+    }
+
+    async avatar() {
+        //debugger
+        let uploads = path.join(process.cwd(), 'uploads');
+        
+        let avatar = this.payload.avatar || 'default_user.png';
+
+        if(this.payload.class === 'Shadow') {
+            return {
+                $redirect: true,
+                url: avatar
+            };
+        }
+        else {
+            avatar = path.join(uploads, !avatar ? 'anonymous' : this.payload._id, avatar);
+
+            return {
+                $sendAsFile: true,
+                file: avatar
+            };
+        }
+
     }
 
     async get() {
